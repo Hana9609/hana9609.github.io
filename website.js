598677100577dcca32401a04a9b9c28a6831b941
Hana9609 <43216101+Hana9609@users.noreply.github.com>
2018-10-23 13:06:54 -0500

    Add files via upload

--- website.js
+++ website.js
@@ -87,3 +87,41 @@ function assignGrade()
     }
 
 }
+
+function tenToSmall(num, base)
+{
+    num = 79;
+    base = 2;
+    smallNum = "";
+
+    while (num > 0)
+    {
+        smallNum = num%base + smallNum; // returns remainder
+        num = Math.floor(num/base);
+        alert(smallNum);
+        alert(num);
+
+    }
+    return smallNum;
+
+}
+
+function smallToTen(num, base)
+{
+    num = 1101;
+    base = 2;
+    tenNum = 0;
+    numLength = num.toString().length;
+
+    while (num > 0)
+    {
+        var denominator = Math.pow(10,(numLength-1));
+        var currentDigit = Math.floor(num/denominator);
+        tenNum = tenNum + currentDigit*Math.pow(base,numLength-1);
+        num = num%denominator;
+        numLength--;
+
+    }
+    return tenNum;
+
+}
